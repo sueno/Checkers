1f954d821b137ef5c9fa05f7008a082eb11fbbc7
OpenShift guest <bohemian916@gmail.com>
2014-05-29 05:24:57 -0400

    29 end

--- php/view/php/group_view.php
+++ php/view/php/group_view.php
@@ -2,7 +2,10 @@
 
         //テスト用配列　
 
-    $BEANS = array("groups"=>  array(array("id"=>1,"name"=>"11期"),array("id"=>2,"name"=>"12期"),array("id"=>3,"name"=>"13期")));
+    $reports = array(   array("content_id"=>1,"title"=>"【日報】20140529", "user_id"=>1,"user_name"=>"百田",    "body"=>"今日は暑かった。日傘ほしい", "date"=>'2014-05-29', "comment_num"=>5),
+                        array("content_id"=>2,"title"=>"【日報】20140530", "user_id"=>2,"user_name"=>"玉井",    "body"=>"今日は寒かった。毛布ほしい", "date"=>'2014-05-30', "comment_num"=>3),
+                        array("content_id"=>3,"title"=>"【日報】20140530", "user_id"=>3,"user_name"=>"佐々木",  "body"=>"今日は適当な日だった"      , "date"=>'2014-05-30', "comment_num"=>10)
+                    );
     
 ?>
 
@@ -51,6 +54,28 @@
             </div>
     
             <div class="body_right">
+            <table border="2">
+                <tr>
+                    <th>タイトル</th><th>コメント数</th><th>投稿日</th><th>作成者</th>
+                </tr>
+                <tr>
+                    <th colspan="4">本文</th>
+                </tr>
+                <?php
+                    foreach($reports as $temp)
+                    {
+                        echo "<tr>";
+                        echo "<td>".$temp["title"]."</td>";
+                        echo "<td>".$temp["comment_num"]."</td>";
+                        echo "<td>".$temp["date"]."</td>";
+                        echo "<td>".$temp["user_name"]."</td></tr>";
+                        echo "<tr><td colspan=\"4\">".$temp["body"]."</td></tr>";
+                    }
+                ?>
+                
+                
+            </table>
+            
             </div>
         </div>
 
