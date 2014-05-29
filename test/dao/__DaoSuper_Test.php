b8bc16b01b997b9c3f15a0173b1cfac309712596
Satoshi Ueno <nohohon1021@gmail.com>
2014-05-28 23:02:53 -0400

    create daoClasses

--- test/dao/__DaoSuper_Test.php
+++ test/dao/__DaoSuper_Test.php
@@ -1,6 +1,21 @@
 <?php
 
+echo "hoge";
 
+$dao = new DaoSuper();
+
+$dao->connect();
+
+$res = $dao->selectTable("groups");
+
+print_r($res);
+
+$dao->insertTable("groups","null,'2000'");
+
+
+$res = $dao->selectTable("groups");
+
+print_r($res);
 
 
 
