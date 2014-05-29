380943856f066674b645913194db508194ac0402
OpenShift guest <xxxg_01_w_endless_waltz@yahoo.co.jp>
2014-05-29 03:52:20 -0400

    action disconinue

---
+++ php/action/UserListAction.php
@@ -0,0 +1,46 @@
+<?php
+
+class UserList implements ActionInterface {
+    public __construct() {
+        
+        try {
+            $this->initAction();
+            $this->saveAction();
+            $this->showAction();
+        }
+        catch(Exception e) {
+            $this->initAction();
+        }
+    }
+    
+    /** 
+     * @Override
+     */
+    public initAction() {
+        
+    }
+    
+    /** 
+     * @Override
+     */
+    public saveAction() {
+        
+    }
+    
+    /** 
+     * @Override
+     */
+    public showAction() {
+        
+    }
+    
+    /** 
+     * @Override
+     */
+    public errorAction(Exception e) {
+        throw new Exception('error');
+    }
+}
+
+
+?>
