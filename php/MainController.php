62036885cd36d4e945aebdfdf89d8f776c6b2fbb
OpenShift guest <xxxg_01_w_endless_waltz@yahoo.co.jp>
2014-05-29 01:44:15 -0400

    LoginAction done

--- php/MainController.php
+++ php/MainController.php
@@ -2,14 +2,24 @@
 
 private $post;
 private $mode;
-$actionObj;
+private $actionObj;
+private $controller = new MainController();
+private $action = $controller->getAction();
 
-class MainContrller {
+try {
+    $action->initAction();
+    $action->saveAction();
+    $action->showAction();
+catch {
+    $action->errorAction(Exception e);
+}
+
+class MainController {
     
     public __construct() {
         $this->$post = htmlspecialchars($_POST);
         $this->$mode = $post["mode"];
-        $actionObj = $this->getAction();
+        $this->actionObj = $this->getAction();
     }
     
     public getAction() {
@@ -19,30 +29,26 @@ class MainContrller {
             case 'signup_confirm':
             case 'signup_complete':
                 require_once('action/LoginAction.php');
-                return new LonginAction($post);
+                return new LonginAction($this->post);
                 break;
             case 'group_reports':
                 require_once('action/ReportListAction.php');
-                return new ReportListAction($post); 
+                return new ReportListAction($this->post); 
                 break;
             case 'individual_reports':
                 require_once('action/ReportWrite.php');
-                return new ReportWrite($post); 
+                return new ReportWrite($this->post);
                 break;
             case 'report_show':
                 require_once('action/CommentShow.php');
-                return new CommentShow($post); 
+                return new CommentShow($this->post);
                 break;
-            case 'group_reports':
+            case 'report_edit':
                 require_once('action/UserList.php');
-                return new ReportListAction($post); 
+                return new UserList($this->post);
                 break;
-
         }
-        
-        
     }
-    
 }
 
 ?>
