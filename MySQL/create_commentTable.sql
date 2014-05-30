b38b06c2f16cb7775df15b8d8e9804ba28b7b688
Satoshi Ueno <student@shinjuku-405-10.local>
2014-05-30 15:10:40 +0900

    bugfix

--- MySQL/create_commentTable.sql
+++ MySQL/create_commentTable.sql
@@ -1,10 +1,10 @@
-create table contents (
+create table comments (
 	id				int			not null	primary key		auto_increment ,
 	content_id		int			not null ,
 	poster			int 		not null ,
 	body			text ,
 	comment_date	date		not null ,
 	delete_flg		boolean 	not null 	default false ,
-	foreign key		(content_id)			references	contains(id) ,
+	foreign key		(content_id)			references	contents(id) ,
 	foreign key		(poster)				references	users(id)
 ) engine=InnoDB;
