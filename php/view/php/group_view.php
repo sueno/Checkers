4b526e4a425fcc3069a3a44096e786834da529e8
OpenShift guest <bohemian916@gmail.com>
2014-05-30 00:57:12 -0400

    group_reports

--- php/view/php/group_view.php
+++ php/view/php/group_view.php
@@ -6,7 +6,20 @@
                         array("content_id"=>2,"title"=>"【日報】20140530", "user_id"=>2,"user_name"=>"玉井",    "body"=>"今日は寒かった。毛布ほしい", "date"=>'2014-05-30', "comment_num"=>3),
                         array("content_id"=>3,"title"=>"【日報】20140530", "user_id"=>3,"user_name"=>"佐々木",  "body"=>"今日は適当な日だった"      , "date"=>'2014-05-30', "comment_num"=>10)
                     );
-    $memberList = array();
+    $memberList = array(array("member_id"=>1,"member_name"=>"百田"), 
+                        array("member_id"=>2,"member_name"=>"玉井"),
+                        array("member_id"=>3,"member_name"=>"佐々木"),
+                        array("member_id"=>4,"member_name"=>"有安"),
+                        array("member_id"=>5,"member_name"=>"高城")
+                    );
+                        
+    $candidateList = array( array("member_id"=>1,"member_name"=>"前田"), 
+                            array("member_id"=>2,"member_name"=>"指原"),
+                            array("member_id"=>3,"member_name"=>"篠田"),
+                            array("member_id"=>4,"member_name"=>"渡辺"),
+                            array("member_id"=>5,"member_name"=>"高橋"));
+    
+    $path="testPOSTview.php";
     
 ?>
 
@@ -19,22 +32,21 @@
         <!-- リンクをクリックした時のPOST送信動作-->
         <script type="text/javascript">
             function bodyClick(content_id){
-                document.bodyForm.mode="report_show";
-                document.bodyForm.content_id = content_id;
+                document.getElementById("content_id_input").value = content_id;
                 document.bodyForm.submit();
+                
             }
             
             function commentClick(content_id){
-                document.commentForm.mode="report_show";
-                document.commentForm.content_id = content_id;
-                document.commentForm.comment = 1;
+                document.getElementById("comment_input1").value = content_id;
                 document.commentForm.submit();
+                
             }
             
             function userClick(user_id){
-                document.userForm.mode="individual_reports";
-                document.userForm.user_id = user_id;
+                document.getElementById("user_input").value= user_id;
                 document.userForm.submit();
+                
             
             }
         </script>
@@ -46,23 +58,19 @@
         <div id="menu">
             <ul>
                 <li><a href="" onclick="window.document.menuForm1.submit(); return false;" >個人ページ</a>
-                    <form name="menuForm1" method="POST" action="testPOSTview.php">
-                        <input type="hidden" name="mode" value="individual_reports">
-                        
+                    <form name="menuForm1" method="POST" action="<?php echo $path; ?>?mode=individual_reports">
                     </form>
                 </li>
 
 
-                <li><a href="" onclick="document.menuForm.submit();return false;">グループページ</a>
-                    <form name="menuForm2" method="POST" action="../../MainController.php">
-                        <input type="hidden" name="mode" value="group_reports">
+                <li><a href="" onclick="document.menuForm2.submit();return false;">グループページ</a>
+                    <form name="menuForm2" method="POST" action="<?php echo $path; ?>?mode=group_reports">
                     </form>
                 </li>
 
 
-                <li><a href="" onclick="document.menuForm.submit(); return false;">日報投稿</a>
-                       <form name="menuForm3" method="POST" action="../../MainController.php">
-                            <input type="hidden" name="mode" value="report_edit">
+                <li><a href="" onclick="document.menuForm3.submit(); return false;">日報投稿</a>
+                       <form name="menuForm3" method="POST" action="<?php echo $path; ?>?mode=report_edit">
                         </form>
                 </li>
  
@@ -76,9 +84,26 @@
             
         <div class="body_part">
         
-        <!-- グループメンバ表示部-->
+            <!-- グループメンバ表示部-->
             <div class="body_left">
-            
+                <h2>メンバー</h2><br>
+                <ul>
+                    <?php
+                        foreach($memberList as $temp)
+                         echo "<li><a href=\"\" onclick=\"userClick(".$temp["member_id"]."); return false;\">".$temp["member_name"]."</a></li>";
+
+                    ?>
+                </ul>
+                
+                 <h2>承認待ちユーザー</h2><br>
+                <ul>
+                    <?php
+                        foreach($candidateList as $temp)
+                         echo "<li>".$temp["member_name"]."　　　　"."<a href=\"\" onclick=\"userClick(".$temp["member_id"]."); return false;\">承認</a></li>";
+
+                    ?>
+                </ul>               
+                
             </div>
     
             <!-- 日報一覧表示部-->
@@ -92,27 +117,28 @@
                 </tr>
                 
                 <!-- POST送信のFORM-->
-                <form name="bodyForm" method="POST" action="../../MainController.php">
-                        <input type="hidden" name="mode" value="report_show">
+                <form name="bodyForm" method="POST" action="<?php echo $path; ?>?mode=report_show">
+                    <input id="content_id_input" type="hidden" name="content_id" value="0">
                 </form>
                 
-                <form name="commentForm" method="POST" action="../../MainController.php">
-                        <input type="hidden" name="mode" value="report_show">
+                <form name="commentForm" method="POST" action="<?php echo $path; ?>?mode=report_show">
+                    <input id="comment_input1" type="hidden" name="content_id" value="0">
+                    <input id="comment_input2" type="hidden" name="comment" value="1">
                 </form>
                 
-                <form name="userForm" method="POST" action="../../MainController.php">
-                        <input type="hidden" name="mode" value="individual_reports">
+                <form name="userForm" method="POST" action="<?php echo $path; ?>?mode=individual_reports">
+                    <input id="user_input" type="hidden" name="user_id" value="0">
                 </form>
                 
                 <?php
                     foreach($reports as $temp)
                     {
                         echo "<tr>";
-                        echo "<td><a href=\"\" onclick=\"bodyClick(".$temp["content_id"].")\">".$temp["title"]."</a></td>";
-                        echo "<td><a href=\"\" onclick=\"commentClick(".$temp["content_id"].")\">".$temp["comment_num"]."</a></td>";
+                        echo "<td><a href=\"\" onclick=\"bodyClick(".$temp["content_id"]."); return false;\">".$temp["title"]."</a></td>";
+                        echo "<td><a href=\"\" onclick=\"commentClick(".$temp["content_id"]."); return false;\">コメント(".$temp["comment_num"].")</a></td>";
                         echo "<td>".$temp["date"]."</td>";
-                        echo "<td><a href=\"\" onclick=\"userClick(".$temp["user_id"].")\">".$temp["user_name"]."</a></td>";
-                        echo "<tr><td colspan=\"4\"><a href=\"\" onclick=\"bodyClick(".$temp["content_id"].")\">".$temp["body"]."</a></td></tr>";
+                        echo "<td><a href=\"\" onclick=\"userClick(".$temp["user_id"]."); return false;\">".$temp["user_name"]."</a></td>";
+                        echo "<tr><td colspan=\"4\"><a href=\"\" onclick=\"bodyClick(".$temp["content_id"]."); return false;\">".$temp["body"]."</a></td></tr>";
                     }
                 ?>
                 
--- php/view/php/testPOSTview.php
+++ php/view/php/testPOSTview.php
@@ -7,17 +7,32 @@
 
 
     <body>
-
+    
+    <h1>GET送信情報一覧</h1>
 <?php
 
-        <table>
+        echo "<table border=1>";
+        foreach($_GET as $key=>$value)
+        {
+            
+             echo "<tr><td>".$key."</td><td>".$value."</td></tr>";
+
+        }
+        echo"</table>";
+        
+?>    
+    <br>
+    <h1>POST送信情報一覧</h1>
+<?php
+
+        echo "<table border=1>";
         foreach($_POST as $key=>$value)
         {
             
              echo "<tr><td>".$key."</td><td>".$value."</td></tr>";
 
         }
-        </table>
+        echo"</table>";
         
 ?>
     </body>
