5b468e2ba8fd2f0287f9451173bf31c82da9aac1
atari <student@shinjuku-405-05.local>
2014-06-02 17:19:55 +0900

    コメント挿入　テスト

---
+++ php/action/CommentAddAction.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'dao/CommentDao.php';
+require_once 'action/ActionSuper.php';
+require_once 'action/ActionInterface.php';
+
+class CommentShowAction extends ActionSuper implements ActionInterface {
+
+    private $commentDaoObj;
+    private $post;
+
+    public function __construct($post) {
+    	parent::__construct($post);
+    	$this->post = $post;
+    	$this->commentDaoObj = new CommentDao();
+    }
+    
+    /**
+     * @Override
+     */
+    public function initAction () {
+        $this->commentDaoObj->connect();
+    }
+    
+    /** 
+     * @Override
+     */
+    public function saveAction() {
+    	$this->loginObj->insert($this->post);
+    }
+    
+    /** 
+     * @Override
+     */
+    public function showAction() {
+    }
+    
+}
+
+?>
