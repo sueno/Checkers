a859da1ce1209d577d8bbb59a8be759369dd04af
Satoshi Ueno <student@shinjuku-405-10.local>
2014-06-03 14:32:14 +0900

    ヘッダー直し

--- php/view/layout/header.php
+++ php/view/layout/header.php
@@ -43,7 +43,7 @@
 
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
+		<!-- Brand and toggle get grouped for better mobile display 
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse"
 				data-target="#bs-example-navbar-collapse-1">
@@ -51,11 +51,12 @@
 			</button>
 			<a class="navbar-brand" href="MainController.php?mode=individual_reports&user_id=<?php echo $session["user_id"]; ?>">個人ページ</a>
 		</div>
+		-->
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="MainController.php?mode=group_reports&user_id=<?php echo $session["user_id"]; ?>" onclick="sendRequest('MainController.php?mode=group_reports',{'group_id':<?php echo $session["group_id"]; ?>})">グループページ</a></li>
+				<li class="active"><a href="MainController.php?mode=group_reports&user_id=<?php echo $session["user_id"]; ?>" >グループページ</a></li>
 				<li><a href="MainController.php?mode=individual_reports&user_id=<?php echo $session["user_id"]; ?>">設定</a></li>
 			</ul>
 		</div>
