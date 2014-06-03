9cb586cfdf781be0ce226904ac337f2bbd163eb7
bohemian916 <student@shinjuku-405-08.local>
2014-06-03 13:24:03 +0900

    一括削除を削除＆日報閲覧の結合

--- php/view/php/report_edit_view.php
+++ php/view/php/report_edit_view.php
@@ -74,9 +74,9 @@
             <table border="1">
                 <form method="POST" action="<?php echo $path; ?>?mode=report_manage">
                 <tr><td>タイトル：</td> <td><input type="text" name="contents_title" value="<?php echo $report["title"]; ?>" size="50"></td>
-                    <td>作成日:</td>    <td><input type="text" name="contents_date" value="<?php echo $report["content_date"]; ?>" size="50"></td></tr>
+                    <td>作成日:</td>    <td><input type="text" name="contents_content_date" value="<?php echo $report["contents_content_date"]; ?>" size="50"></td></tr>
                     
-                <tr><td colspan="4"><textarea name="body"  rows="30" cols="100"><?php echo $report["body"]; ?></textarea></td></tr>
+                <tr><td colspan="4"><textarea name="contents_body"  rows="30" cols="100"><?php echo $report["body"]; ?></textarea></td></tr>
             </table>
             <INPUT TYPE="HIDDEN" NAME="contents_id" VALUE="<?php ECHO $report["contents_id"];?>">
             <input type="submit" value="投稿する"></form>
@@ -86,10 +86,10 @@
         {?>
         	<table border="1">
         	<form method="POST" action="<?php echo $path; ?>?mode=report_manage">
-        	<tr><td>タイトル：</td> <td><input type="text" name="title"  size="50"></td>
-        	<td>作成日:</td>    <td><input type="text" name="content_date" value="<?php echo date('Y-m-d'); ?>" size="50"></td></tr>
+        	<tr><td>タイトル：</td> <td><input type="text" name="contents_title"  size="50"></td>
+        	<td>作成日:</td>    <td><input type="text" name="contents_content_date" value="<?php echo date('Y-m-d'); ?>" size="50"></td></tr>
         	
-        	<tr><td colspan="4"><textarea name="body"  rows="30" cols="100">本文をここに入力</textarea></td></tr>
+        	<tr><td colspan="4"><textarea name="contents_body"  rows="30" cols="100">本文をここに入力</textarea></td></tr>
         	</table>
         	
         	<input type="submit" value="投稿する"></form>
