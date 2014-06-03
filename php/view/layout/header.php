685f53a1ccb320a789d5bd3dec9917bc9151e301
Satoshi Ueno <student@shinjuku-405-10.local>
2014-06-03 13:25:27 +0900

    ヘッダーつくってる途中

--- php/view/layout/header.php
+++ php/view/layout/header.php
@@ -1,46 +1,63 @@
 
+<link href="view/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link href="view/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" />
+
+
+<script src="view/js/jquery-1.9.1.min.js"></script>
+<script src="view/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+				function sendRequest (url, post) {
+					console.log(post);
+					$.ajax({
+						url		: url,
+						async	: false,	
+						type	: "POST",
+						data	: post,
+						dataType: 'html',
+						error	: function(XMLHttpRequest){
+				            console.log(XMLHttpRequest);
+						},
+						success	: function(data,status){
+							var nextPage = data;
+// 							changeContents(nextPage);
+// 							var state = $(this).attr("title")
+// 							window.history.pushState(state, null, url);
+	 						document.write(data);
+						}
+					});
+				}
+				//コンテンツの切り替え
+				function changeContents(url) {
+					$('article').load(url+' section');
+				}
+
+			$(function() {
+				//戻る・進むボタンを押したとき
+				onpopstate = function(event) {
+					changeContents(location.pathname);
+				}
+			});
+</script>
+
+<?php $session = $_SESSION; ?>
+
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse"
 				data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span> <span
-					class="icon-bar"></span> <span class="icon-bar"></span> <span
-					class="icon-bar"></span>
+				<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">個人ページ</a>
+			<a class="navbar-brand" href="#" onclick="sendRequest('MainController.php?mode=individual_reports',{'user_id':<?php echo $session["user_id"]; ?>})">個人ページ</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse"
-			id="bs-example-navbar-collapse-1">
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">グループページ</a></li>
-				<li><a href="#">設定</a></li>
-				<li class="dropdown"><a href="#" class="dropdown-toggle"
-					data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Separated link</a></li>
-						<li class="divider"></li>
-						<li><a href="#">One more separated link</a></li>
-					</ul></li>
+				<li class="active"><a href="#" onclick="sendRequest('MainController.php?mode=group_reports',{'group_id':<?php echo $session["group_id"]; ?>})">グループページ</a></li>
+				<li><a href="#" onclick="sendRequest('MainController.php?mode=individual_reports',{'user_id':<?php echo $session["user_id"]; ?>})">設定</a></li>
 			</ul>
-			<form class="navbar-form navbar-right" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="user name">
-				</div>
-				<div class="form-group">
-					<input type="password" class="form-control" placeholder="password"
-						name="users_password">
-				</div>
-				<button type="submit" class="btn btn-default">Sign in</button>
-			</form>
-
 		</div>
 		<!-- /.navbar-collapse -->
 	</div>
