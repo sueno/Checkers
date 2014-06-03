abb6ae779fb4de5cfdc91fbff09a3dc369d1f1f2
bohemian916 <student@shinjuku-405-08.local>
2014-06-03 14:14:05 +0900

    グループビューコメント綺麗に

--- php/view/php/group_view.php
+++ php/view/php/group_view.php
@@ -1,43 +1,20 @@
 <?php $DEBUG = 1; 
 
-        //テスト用配列　本番はこれらの宣言をコメントアウト
-//     $session = array("user_name"=>"有安杏果","group_name"=>"１１期", "user_id"=>3,"group_id"=>1); 
-    
-//     $reports = array(   array("content_id"=>1,"title"=>"【日報】20140529", "user_id"=>1,"user_name"=>"百田",    "body"=>"今日は暑かった。日傘ほしい", "content_date"=>'2014-05-29', "comment_num"=>5),
-//                         array("content_id"=>2,"title"=>"【日報】20140530", "user_id"=>2,"user_name"=>"玉井",    "body"=>"今日は寒かった。毛布ほしい", "content_date"=>'2014-05-30', "comment_num"=>3),
-//                         array("content_id"=>3,"title"=>"【日報】20140530", "user_id"=>3,"user_name"=>"佐々木",  "body"=>"今日は適当な日だった"      , "content_date"=>'2014-05-30', "comment_num"=>10)
-//                     );
-//     $memberList = array(array("member_id"=>1,"member_name"=>"百田"), 
-//                         array("member_id"=>2,"member_name"=>"玉井"),
-//                         array("member_id"=>3,"member_name"=>"佐々木"),
-//                         array("member_id"=>4,"member_name"=>"有安"),
-//                         array("member_id"=>5,"member_name"=>"高城")
-//                     );
-                        
-//     $candidateList = array( array("member_id"=>1,"member_name"=>"前田"), 
-//                             array("member_id"=>2,"member_name"=>"指原"),
-//                             array("member_id"=>3,"member_name"=>"篠田"),
-//                             array("member_id"=>4,"member_name"=>"渡辺"),
-//                             array("member_id"=>5,"member_name"=>"高橋"));
+
  $reports = $BEANS["reports"];
  $member = $BEANS["member"];
  $candidate = $BEANS["candidate"];
  
-//  var_dump($BEANS);
     
      //パス指定
-//  $path="testPOSTview.php";
     $path="MainController.php";
-  
-//    $headPath="../css/";
     $headPath="view/css/"; 
 
     //BEANSを代入
     $userInfo = $_SESSION;   //$_SESSION
     $memberList = $member;
     $candidateList = $candidate;
-    
-    
+      
 ?>
 
 <html>
@@ -78,7 +55,7 @@
         <div id="menu">
             <ul>
                 <li><a href="" onclick="window.document.menuForm1.submit(); return false;" >個人ページ</a>
-                    <form name="menuForm1" method="POST" action="<?php echo $path; ?>?mode=individual_reports">
+                    <form name="menuForm1" method="POST" action="<?php echo $path; ?>?mode=individual_reports&user_id=<?php echo $userInfo["user_id"]; ?>">
                     <input type="hidden" name="user_id" value="<?php echo $userInfo["user_id"]; ?>">
                     </form>
                 </li>
